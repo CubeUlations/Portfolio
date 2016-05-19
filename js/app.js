8fd26b4542ab00798326940f45e5f131dc9d9270
Ralph Maamari <ralphpal@hotmail.com>
2016-05-19 00:12:12 -0400

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-//This will contain some JS code
-document.getElementById('btn').onclick = function(){
-		var src = 'http://upload.wikimedia.org/wikipedia/commons/4/4b/Apple_pie.jpg',
-			img = document.createElement('img');
-
-		img.src = src;
-		document.getElementById('image').appendChild(img);
-}
